ec0950c4096eed9b61803a35eb61fb112b1c6c6f
Marc Brown <marcsmobile.brown@googlemail.com>
2025-02-15 23:02:56 +0000

    Create known_caller_intents.py

---
+++ config/known_caller_intents.py
@@ -0,0 +1,7 @@
+intents:
+  speak_to_dad:
+    prompt: "Transferring your call to Dad."
+    extension: "200"
+  speak_to_browny:
+    prompt: "Transferring your call to Browny."
+    extension: "300"
